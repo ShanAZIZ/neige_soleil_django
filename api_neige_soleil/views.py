336d241d3e10c508c8ed179d415f1629bd077e37
Shan <shan.azizh@gmail.com>
2021-03-30 00:51:26 +0200

    Réecriture des classes avec l'authentification Basic et Token, et Permission isAdmin

--- api_neige_soleil/views.py
+++ api_neige_soleil/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
 
 from .serializers import *
 from neige_soleil_app.models import *
@@ -10,39 +10,36 @@ from neige_soleil_app.models import *
 # TODO: API - Adapter les permissions des vues
 
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
-    permission_classes = [IsAuthenticated]
+class AdminViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
     authentication_classes = [BasicAuthentication, TokenAuthentication]
+
+
+class UserViewSet(AdminViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class ProfileViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+class ProfileViewSet(AdminViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
 
-class ProfileProprietaireViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+class ProfileProprietaireViewSet(AdminViewSet):
     queryset = ProfileProprietaire.objects.all()
     serializer_class = ProfileProprietaireSerializer
 
 
-class ContratProprietaireViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+class ContratProprietaireViewSet(AdminViewSet):
     queryset = ContratProprietaire.objects.all()
     serializer_class = ContratProprietaireSerializer
 
 
-class ReservationViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+class ReservationViewSet(AdminViewSet):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
 
 
-class LocationViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+class LocationViewSet(AdminViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
