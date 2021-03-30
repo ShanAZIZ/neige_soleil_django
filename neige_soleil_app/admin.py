833d8875de820ac7bc28e3786c508263ae48c725
Shan <shan.azizh@gmail.com>
2021-03-31 00:41:53 +0200

    Ajout de profile dans l'interface administrateur

--- neige_soleil_app/admin.py
+++ neige_soleil_app/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Utilisateur)
+admin.site.register(Profile)
 admin.site.register(Reservation)
 admin.site.register(ContratProprietaire)
 admin.site.register(ProprieteImage)
